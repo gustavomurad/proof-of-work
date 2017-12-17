ca7a9e9af787a8fcc17f1ca2bda10f2243b71df0
Gustavo Murad <gustavomurad@gmail.com>
2017-12-17 17:04:30 -0200

    First Commit

---
+++ App.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const SHA256 = require('crypto-js/sha256');
+const DIFFICULT  = 4;
+
+class User{
+    constructor(displayName, email, password, nonce = 0, proofOfWork = ''){
+        this.displayName = displayName;
+        this.email = email;
+        this.password = password;
+        this.nonce = nonce;
+        this.proofOfWork = proofOfWork;
+    }
+
+    get JSON() {
+        return JSON.stringify({
+            displayName: this.displayName, email: this.email, password: this.password, nonce: this.nonce, proofOfWork: this.proofOfWork
+        });
+    }
+    
+    static fromJSON(json) {
+        var data = JSON.parse(json);
+        var user = new User(data.displayName, data.email, data.password, data.nonce, data.proofOfWork);
+        return user;
+    }
+}
+
+let calculateProofOfWork = (user, difficult) => {
+    while(user.proofOfWork.substring(0, difficult) !== "0".repeat(difficult)) {
+        user.nonce++;
+        user.proofOfWork = SHA256(user.displayName + user.email + user.password +user.nonce).toString();
+        console.log("Calculating: " + user.proofOfWork);
+        console.log("Nonce: " + user.nonce);
+    }
+
+    console.log("Proof of Work: " + user.proofOfWork);
+    return user;
+};
+
+let validateProofofWork = (user) => {
+    return (user.proofOfWork === SHA256(user.displayName + user.email + user.password +user.nonce).toString())? true : false;
+};
+
+//Recive a new user from login form, and calculate the prrof of work, 
+//this will make the work of a robot more difficult;
+let user = new User('Satoshi Nakamoto', 'snakamoto@bitcoin.org', 'asdf1234' );
+console.log(user.JSON);
+
+//calculate Proof of Work
+user = calculateProofOfWork(user, DIFFICULT);
+
+//Now, you can send to server this data, and validate on server side
+console.log(user.JSON);
+
+//validate on server like this
+console.log('Is valid? ' + validateProofofWork(user));
---
+++ README.md
@@ -0,0 +1,10 @@
+Simple demonstration in JS of how a PoW (Proof of Work) works
+
+Nonce = "The nonce in a bitcoin block is a 32-bit (4-byte) field whose value is set so that the hash of the block will contain a run of leading zeros. The rest of the fields may not be changed, as they have a defined meaning." https://en.bitcoin.it/wiki/Nonce
+
+Proof of work = "A proof of work is a piece of data which is difficult (costly, time-consuming) to produce but easy for others to verify and which satisfies certain requirements. Producing a proof of work can be a random process with low probability so that a lot of trial and error is required on average before a valid proof of work is generated. Bitcoin uses the Hashcash proof of work system." https://en.bitcoin.it/wiki/Proof_of_work#Traditional_proof_of_work
+
+
+To install type: npm install 
+
+To run application type: npm start
